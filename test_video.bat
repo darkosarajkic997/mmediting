772791b62914c7404d0de152b2790de942077ca6
darko <darkosarajkic997@gmail.com>
2022-03-29 11:30:41 +0200

    large file removed

---
+++ test_video.bat
@@ -0,0 +1,6 @@
+python demo/restoration_video_demo.py ^
+configs/restorers/real_basicvsr/realbasicvsr_c64b20_1x30x8_lr5e-5_150k_reds.py ^
+checkpoints\RealBasicVSR\realbasicvsr_c64b20_1x30x8_lr5e-5_150k_reds_20211104-52f77c2c.pth  ^
+data/TrackedPlates.mp4 ^
+results/demo_tracked_plates.mp4 
+
